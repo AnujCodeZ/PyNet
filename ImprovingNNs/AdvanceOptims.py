d67e0540ed9bda48e757fbadfc9bd9f75d8b7568
AnujCodeZ <anujrana.sid55@gmail.com>
2020-08-15 21:09:26 +0530

    RMSProp

--- ImprovingNNs/AdvanceOptims.py
+++ ImprovingNNs/AdvanceOptims.py
@@ -27,3 +27,32 @@ class GDWithMomentum(DeepNet):
             
             self.parameters['W'+str(i+1)] -= lr * self.averages['W'+str(i+1)]
             self.parameters['b'+str(i+1)] -= lr * self.averages['b'+str(i+1)]
+
+class RMSProp(DeepNet):
+    
+    def __init__(self, beta=0.99):
+        super(DeepNet, self).__init__()
+        self.beta = beta
+        self.averages = {}
+        self.init_averages()
+        
+    def init_averages(self):
+        
+        for i in range(len(self.layers)):
+            
+            self.averages['W'+str(i+1)] = np.zeros(self.parameters['W'+str(i+1)].shape)
+            self.averages['b'+str(i+1)] = np.zeros(self.parameters['b'+str(i+1)].shape)
+    
+    def update_params_with_rmsprop(self, lr=3e-3):
+        
+        for i in range(len(self.layers)):
+            
+            self.averages['W'+str(i+1)] = (np.multiply(self.beta, self.averages['W'+str(i+1)]) + 
+                                           np.multiply((1 - self.beta), np.square(self.gradients['W'+str(i+1)])))
+            self.averages['b'+str(i+1)] = (np.multiply(self.beta, self.averages['b'+str(i+1)]) + 
+                                           np.multiply((1 - self.beta), np.square(self.gradients['b'+str(i+1))]))
+            
+            self.parameters['W'+str(i+1)] -= lr * np.divide(self.gradients['W'+str(i+1)],
+                                                            np.sqrt(self.averages['W'+str(i+1)]))
+            self.parameters['b'+str(i+1)] -= lr * np.divide(self.gradients['b'+str(i+1)],
+                                                            np.sqrt(self.averages['b'+str(i+1)]))
